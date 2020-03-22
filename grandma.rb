0178ae1fd8792f52cf766beec2b0e5658aa494be
Blend Ibraj <blendi.ibraj96@gmail.com>
2020-03-22 16:17:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,5 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase = "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase = "Hi Nana, how are you?" || "Hi!"
-    puts "HUH?! SPEAK UP, SONNY"
-  elsif phrase = "WHAT DID YOU EAT TODAY" || 'WHAT?'
-    puts "NO, NOT SINCE 1938!"
-  end
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
